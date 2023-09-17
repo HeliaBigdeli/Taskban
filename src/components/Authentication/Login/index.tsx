05933aecdf6ba4b6074669c2007ca6263baf9c20
Aliarm79 <104495905+Aliarm79@users.noreply.github.com>
2023-09-17 23:04:03 +0330

    login component added

--- src/components/Authentication/Login/index.tsx
+++ src/components/Authentication/Login/index.tsx
@@ -1,11 +1,42 @@
 import AuthLayout from "../../Layouts/Auth";
+import styles from "./style.module.css";
 
 const Login = () => {
-    return (
-          <AuthLayout>
-                  login
-          </AuthLayout>
-    );
-}
-  
+  return (
+    <AuthLayout>
+      <div className={styles.cardContainer}>
+        <h2 className={styles.cardHeading}>
+          {"(: به کوئرا تسک منیجر خوش برگشتی"}
+        </h2>
+        <section className={styles.inputContainer}>
+          <div className={styles.inputs}>
+            <div className={styles.inputFiled}>
+              <h4 className={styles.inputHeader}>ایمیل</h4>
+              <input className={styles.input} type="email" />
+            </div>
+            <div className={styles.inputFiled}>
+              <h4 className={styles.inputHeader}>رمز عبور</h4>
+              <input className={styles.input} type="password" />
+              <span className={styles.passwordReminder}>
+                رمز عبور را فراموش کرده‌ای؟
+              </span>
+            </div>
+          </div>
+          <div className={styles.buttons}>
+            <button className={styles.loginButton} type="submit">
+              <span className={styles.buttonText}>ورود</span>
+            </button>
+            <div className={styles.goRegister}>
+              <span className={styles.goRegisterText}>ثبت‌نام</span>
+              <span className={styles.goRegisterQuestion}>
+                ثبت‌نام نکرده‌ای؟
+              </span>
+            </div>
+          </div>
+        </section>
+      </div>
+    </AuthLayout>
+  );
+};
+
 export default Login;

3cb8a1e6c96785575cc707a97f798ce0254054fb
Aliarm79 <104495905+Aliarm79@users.noreply.github.com>
2023-09-21 19:08:12 +0330

    complete forgot component

--- src/components/Authentication/ForgotPassword/index.tsx
+++ src/components/Authentication/ForgotPassword/index.tsx
@@ -1,14 +1,35 @@
+import { useState } from "react";
 import AuthLayout from "../../Layouts/Auth";
 import Card from "../../Layouts/Auth/Card";
 
-const ForgotPassword :React.FC = () :JSX.Element => {
+const ForgotPassword: React.FC = (): JSX.Element => {
+  const [isSent, setIsSent] = useState<boolean>(false);
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setIsSent((isSent) => !isSent);
+  };
   return (
     <AuthLayout>
       <Card page={"forgot"}>
-        <section className="flex flex-col items-end gap-L self-stretch">
-          <form className="flex flex-col items-end gap-5 self-stretch ">
-            <div className="flex flex-col items-end gap-XS self-stretch">
-              <label className="text-black text-sm font-normal leading-normal" htmlFor="email">
+        <section
+          className={`flex flex-col ${isSent ? "items-center" : "items-end"} ${
+            isSent && "justify-center"
+          } gap-L self-stretch`}
+        >
+          <form
+            className={`flex flex-col ${
+              isSent ? "items-center" : "items-end"
+            } gap-5 self-stretch `}
+          >
+            <div
+              className={`${
+                isSent ? "hidden" : "flex"
+              }  flex-col items-end gap-XS self-stretch`}
+            >
+              <label
+                className="text-black text-sm font-normal leading-normal"
+                htmlFor="email"
+              >
                 ایمیل خود را وارد کنید
               </label>
               <input
@@ -19,11 +40,22 @@ const ForgotPassword :React.FC = () :JSX.Element => {
               />
             </div>
             <button
+              onClick={handleClick}
               type="submit"
-              className="text-white text-sm leading-normal font-extrabold h-12 self-stretch rounded-md bg-brand-primary"
+              className={`${
+                isSent && "hidden"
+              } text-white text-sm leading-normal font-extrabold h-12 self-stretch rounded-md bg-brand-primary`}
             >
-                دریافت ایمیل بازیابی رمز عبور
+              دریافت ایمیل بازیابی رمز عبور
             </button>
+            <span
+              className={`${
+                !isSent && "hidden"
+              } text-black text-sm font-normal leading-normal`}
+            >
+              .لینک بازیابی رمز عبور برای شما ایمیل شد. لطفا ایمیل خود را بررسی
+              کنید
+            </span>
           </form>
         </section>
       </Card>

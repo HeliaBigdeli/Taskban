d95feea6d3ad2df7eabbf9fcaf938b264a09b03a
unknown <f.e.h.farshad@gmail.com>
2023-10-01 21:13:33 +0330

    Icon usage fixed in profile

--- src/components/Layouts/Profile/index.tsx
+++ src/components/Layouts/Profile/index.tsx
@@ -1,40 +1,49 @@
 import Button from "../../Common/Form/Button";
 import SideBar from "../Dashboard/SideBar";
-import styles from "../Auth/Header/style.module.css"
+import styles from "../Auth/Header/style.module.css";
 import { Outlet } from "react-router-dom";
-import IconItem from "../../Common/IconItem";
-const ProfileLayout :React.FC = ():JSX.Element => {
-  const handleClick = () => {
-
-  };
+import Icon from "../../Common/Icon";
+const ProfileLayout: React.FC = (): JSX.Element => {
+  const handleClick = () => {};
   return (
     <div className="flex px-2XL flex-row-reverse">
-      <SideBar >
-      <h2 className={`${styles.navbarTitle} mb-[75px] mt-XL`}>
+      <SideBar>
+        <h2 className={`${styles.navbarTitle} mb-[75px] mt-XL`}>
           کوئرا تسک منیجر
         </h2>
         <div className="flex flex-col items-end ">
-        <Button text="بازگشت" type="button" hasIcon={true} onClick={handleClick}
-          className="bg-brand-primary text-white h-L text-xl rounded-md w-[113px] justify-start mb-[36px] p-[4px]"
-          icon={{
-            icon: "chevron_right",
-            color: "white",
-            className: "ml-1"
-          }}/>
+          <Button
+            text="بازگشت"
+            type="button"
+            hasIcon={true}
+            onClick={handleClick}
+            className="bg-brand-primary text-white h-L text-xl rounded-md w-[113px] justify-start mb-[36px] p-[4px]"
+            icon={{
+              icon: "chevron_right",
+              color: "white",
+              className: "ml-1",
+            }}
+          />
 
-         <ul className="w-full justify-start">
-          <IconItem text="اطلاعات فردی" url="information" icon="edit"></IconItem>
-          <IconItem text="اطلاعات حساب" url="account" icon="edit"></IconItem>
-          <IconItem text="تنظیمات" url="setting" icon="setting"></IconItem>
-        </ul>
-          </div>
-          
+          <ul className="w-full justify-start">
+            <span>
+              اطلاعات فردی
+              <Icon icon="edit" />
+            </span>
+            <span>
+              اطلاعات حساب <Icon icon="user" />
+            </span>
+            <span>
+              تنظیمات <Icon icon="setting" />
+            </span>
+          </ul>
+        </div>
       </SideBar>
       <div>
-        <Outlet/>
+        <Outlet />
       </div>
     </div>
   );
-}
+};
 
 export default ProfileLayout;

5363fc9dce788ea06fe473f1fb9c973efa0211c8
Mahiisaa <mahsababaiee93@gmail.com>
2023-10-03 16:39:39 +0330

    focus style added to profileLayout List

--- src/components/Common/IconItem/index.tsx
+++ src/components/Common/IconItem/index.tsx
@@ -9,7 +9,7 @@ interface IProps {
 const IconItem: React.FC<IProps> = ({ url, text, icon }): JSX.Element => {
   return (
     <li>
-      <Link to={`/${url}`} className="flex flex-row justify-end items-center">
+      <Link to={`/${url}`} className="flex flex-row justify-end items-center py-[4px] px-[8px] focus:bg-blue-secondary focus:font-extrabold">
         <h3 className="mr-[11px] text-xl">{text}</h3>
         <Icon icon={icon}></Icon>
       </Link>

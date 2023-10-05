559ff10320f236ba5ce41390ad207fed46d58562
Mahiisaa <mahsababaiee93@gmail.com>
2023-10-05 15:33:24 +0330

    select tag dropdown added to taskModal

--- src/components/Dashboard/TaskModal/index.tsx
+++ src/components/Dashboard/TaskModal/index.tsx
@@ -5,7 +5,8 @@ import Icon from "../../Common/Icon";
 import { useState } from "react";
 import DatePickerModal from "../DatePickerModal";
 import Textarea from "../../Common/Form/Textarea";
-
+import Select from "../../Common/Form/Select";
+import Input from "../../Common/Form/Input";
 const portals = document.getElementById("portals") as Element;
 
 interface IProps {
@@ -14,7 +15,15 @@ interface IProps {
 }
 
 const TaskModal: React.FC<IProps> = ({ modal, setModal }): JSX.Element => {
+  const tag = [
+    { id: 1, title: "درس", color: "indigo_secondary" },
+    { id: 2, title: "کار", color: "blue_secondary" },
+    { id: 3, title: "پروژه", color: "indigo_secondary" },
+  ];
   const [datePickerModal, setDatePickerModal] = useState<boolean>(false);
+  const [tagDropdown, setTagDropdown] = useState<boolean>(false);
+  const [data, setData] = useState(tag)
+  const [priorityModal, setPriorityModal] = useState<boolean>(false);
   const [values, setVlaues] = useState<{}>({
     description: ""
   })
@@ -22,16 +31,28 @@ const TaskModal: React.FC<IProps> = ({ modal, setModal }): JSX.Element => {
   const handleDatePickerModal = () => {
     setDatePickerModal(!datePickerModal);
   };
+  const handlePriorityModal = () => {
+    setPriorityModal(!priorityModal);
+  };
 
   const handleShowModal = () => {
     setModal(!modal);
   };
+ const dropdownToggele=()=>{
+  setTagDropdown(!tagDropdown)
+ }
+ const handleSearch = (value: string) => {
+  const data = value? tag.filter((item) => {
+    return item.title.includes(value) 
+  }):tag
+  setData(data)
+}
 
   return (
     <>
       {createPortal(
         <Modal
-          style={{}}
+          style={{padding:"32px"}}
           modal={modal}
           setModal={handleShowModal}
           hasHeader={true}
@@ -43,7 +64,7 @@ const TaskModal: React.FC<IProps> = ({ modal, setModal }): JSX.Element => {
           coloredSquare="lightgray_300"
           closeIcon={{ order: 1 }}
         >
-          <div className="flex flex-col w-[1153px] gap-[40px] px-[12px] pb-[12px]">
+          <div className="flex flex-col w-[1153px] gap-[40px]">
             <div className="flex flex-row-reverse items-center gap-[8px]">
               <span>در</span>
               <div className="w-[158px] h-[37px] rounded-md border border-[#E9EBF0] text-right py-[8px] pr-[5px] pl-[4px]">پروژه اول</div>
@@ -64,15 +85,41 @@ const TaskModal: React.FC<IProps> = ({ modal, setModal }): JSX.Element => {
             </div>
             <div className="flex flex-row-reverse justify-between">
               <div className="flex flex-row gap-[24px]">
-                <div className=" cursor-pointer border-dashed border-2 rounded-full border-[#c1c1c1] w-[50px] h-[50px] flex justify-center items-center">
-                  <Icon icon="tag" color="#c1c1c1" />
+                <div className="cursor-pointer border-dashed border-2 rounded-full border-[#c1c1c1] w-[50px] h-[50px] flex justify-center items-center relative"onClick={dropdownToggele}>
+                    <Icon icon="tag" color="#c1c1c1" />
+                    <div id="myDropdown" className={`flex flex-col gap-XS ${tagDropdown? "inline-block":"hidden"} absolute overflow: auto w-[173px] h-[160px] z-2 bg-white p-XS bottom-10 shadow-select rounded-lg`} >
+                      <div>
+                      <Input
+                           autoFocus={true}
+                           className="pr-L border-none bg-lightgray_200 h-XL outline-none placeholder-text-xs h-[32px]"
+                           placeholder="جستجو یا ساختن تگ"
+                           name="search"
+                           id="search"
+                           type="text"
+                           hasLabel={false}
+                           hasIcon={true}
+                           icon={{
+                             icon: "search",
+                             color: "#BDBDBD",
+                             size:24
+                           }}
+                           onChange={handleSearch}
+                             />
+                     
+                      </div>
+                      {data.map((item)=>
+                        <div className="flex flex-row-reverse justify-between" >
+                          <span className={`bg-${item.color} text-${item.color} px-XS rounded-[14px]`}>{item.title}</span>
+                          <Icon icon="dots"></Icon></div>
+                      )}
+                    </div>
                 </div>
                 <div
                   onClick={handleDatePickerModal}
                   className="cursor-pointer border-dashed border-2 rounded-full border-[#c1c1c1] w-[50px] h-[50px] flex justify-center items-center">
                   <Icon icon="calender_full" color="#c1c1c1" />
                 </div>
-                <div className="cursor-pointer border-dashed border-2 rounded-full border-[#c1c1c1] w-[50px] h-[50px] flex justify-center items-center">
+                <div onClick={handlePriorityModal} className="cursor-pointer border-dashed border-2 rounded-full border-[#c1c1c1] w-[50px] h-[50px] flex justify-center items-center">
                   <Icon icon="flag" color="#c1c1c1" />
                 </div>
               </div>
@@ -93,7 +140,7 @@ const TaskModal: React.FC<IProps> = ({ modal, setModal }): JSX.Element => {
         </Modal>,
         portals
       )}
-      {DatePickerModal && <DatePickerModal modal={datePickerModal} setModal={handleDatePickerModal} />}
+      {datePickerModal && <DatePickerModal modal={datePickerModal} setModal={handleDatePickerModal} />}
     </>
   );
 };

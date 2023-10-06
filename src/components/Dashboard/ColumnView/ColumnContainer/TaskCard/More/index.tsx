7efafd59ddbf0de367f3a006516555a41a53a6a0
unknown <f.e.h.farshad@gmail.com>
2023-10-06 14:48:37 +0330

    fix: more component events

--- src/components/Dashboard/ColumnView/ColumnContainer/TaskCard/More/index.tsx
+++ src/components/Dashboard/ColumnView/ColumnContainer/TaskCard/More/index.tsx
@@ -1,20 +1,33 @@
 import Icon from "../../../../../Common/Icon";
+import TaskInfoModal from "../../../../TaskInfoModal";
+import { useState } from "react";
 
 interface IMoreProps {
   isShown: boolean;
 }
+
 const More: React.FC<IMoreProps> = ({ isShown }): JSX.Element => {
+  const [taskModalModal, setDatePickerModal] = useState<boolean>(false);
+  const handleTaskModalModal = () => {
+    setDatePickerModal(!taskModalModal);
+  };
   return (
     <div
       className={`${!isShown ? "h-0" : "h-10"} overflow-hidden transition-all `}
     >
       <hr className="w-[217px] h-[1px] bg-[#EFF0F0] mb-S"/>
-      <section className="flex justify-between items-center self-stretch ">
-          <Icon size={20} icon="dots"/>
+      <section className="flex justify-between items-center self-stretch" onClick={handleTaskModalModal}>
+          <Icon size={20} icon="dots" className="cursor-pointer"/>
         <div className="flex w-4 h-4 justify-center items-center">
-          <Icon size={20} icon="check_circle"/>
+          <Icon size={20} icon="check_circle" className="cursor-pointer"/>
         </div>
       </section>
+      {taskModalModal && (
+        <TaskInfoModal
+          modal={taskModalModal}
+          setModal={handleTaskModalModal}
+        />
+      )}
     </div>
   );
 };

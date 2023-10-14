fc257d6ac167ecf2fc24692947c71819cbf0b189
farshad hosseini <farshadhosseini@alborz.local>
2023-10-14 16:58:28 +0330

    working on axios custom hook

--- src/utils/validator/messages.ts
+++ src/utils/validator/messages.ts
@@ -1,31 +1,31 @@
 export const requiredMessage = (fieldName: string) => {
-    return `.وارد کردن ${fieldName} الزامی است`;
+    return `وارد کردن ${fieldName} الزامی است.`;
 }
 
 export const minLengthMessage = (fieldName: string, n: number) => {
-    return  `.${fieldName} باید حداقل ${n} کاراکتر باشد`;
+    return  `${fieldName} باید حداقل ${n} کاراکتر باشد.`;
 }
 
 export const minMessage = (fieldName: string, n: number) => {
-    return `.${fieldName} باید بزرگتر از ${n} باشد`;
+    return `${fieldName} باید بزرگتر از ${n} باشد.`;
 }    
 
 export const maxMessage = (fieldName: string, n: number) => {
-    return `.${fieldName} باید کوچکتر از ${n} باشد`;
+    return `${fieldName} باید کوچکتر از ${n} باشد.`;
 }
 
 export const emailMessage = (fieldName: string) => {
-    return  `.آدرس ${fieldName} معتبر نیست`;
+    return  `آدرس ${fieldName} معتبر نیست.`;
 }   
 
 export const checkedMessage = (fieldName: string) => {
-    return  `.${fieldName} باید پذیرفته شود`;
+    return  `${fieldName} باید پذیرفته شود.`;
 }   
 
 export const strongMessage = (fieldName: string) => {
-    return  `.${fieldName} باید شامل حروف، اعداد و علامت باشد`;
+    return  `${fieldName} باید شامل حروف، اعداد و علامت باشد.`;
 }   
 
 export const equalMessage = (fieldName: string, extraValue: string) => {
-    return `.${fieldName} باید برابر با ${extraValue} باشد`;
+    return `${fieldName} باید برابر با ${extraValue} باشد.`;
 }   

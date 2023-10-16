ea41d4afa9791c074dfd86a24e900c270f672218
Aliarm79 <104495905+Aliarm79@users.noreply.github.com>
2023-10-16 20:17:50 +0330

    add profileimage to listview

--- src/components/Dashboard/ListView/TaskList/ListItem/index.tsx
+++ src/components/Dashboard/ListView/TaskList/ListItem/index.tsx
@@ -1,16 +1,19 @@
 import Icon from "../../../../Common/Icon";
+import ProfileImage from "../../../../Common/ProfileImage";
 import UserIcon from "../../../../Common/UserIcon";
 import ListItemTitle from "./ListItemTitle";
+import { useSelector } from "react-redux";
 
 interface IListItemProps {
   color?: string;
-  name:string
+  name: string;
 }
-const ListItem: React.FC<IListItemProps> = ({ color,name }): JSX.Element => {
+const ListItem: React.FC<IListItemProps> = ({ color, name }): JSX.Element => {
   const colorVariants = {
     orange: "bg-orange-primary ",
     green: "bg-green-primary",
   };
+  const { first_name, last_name } = useSelector((store: any) => store.auth);
   return (
     <div className="flex w-[986px] py-[7px] justify-between items-center mr-auto">
       <section className="flex items-start gap-[7px]">
@@ -20,14 +23,22 @@ const ListItem: React.FC<IListItemProps> = ({ color,name }): JSX.Element => {
           }`}
         ></div>
 
-        <span className="text-[#0E0E0E] text-xs font-normal">
-         {name}
-        </span>
+        <span className="text-[#0E0E0E] text-xs font-normal">{name}</span>
       </section>
       <section className="flex items-center gap-[70px]">
         <div className="flex w-[70px] justify-center items-center relative ">
-          <UserIcon left={0} />
-          <UserIcon left={30} />
+          <ProfileImage
+            firstName={first_name}
+            lastName={last_name}
+            size={30}
+            nameColor={true}
+          />
+          <ProfileImage
+            firstName={first_name}
+            lastName={last_name}
+            size={30}
+            nameColor={true}
+          />
         </div>
 
         <ListItemTitle title={"۶ آبان"} />

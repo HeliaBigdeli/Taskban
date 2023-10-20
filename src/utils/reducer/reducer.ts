5fd107a4ddbe4efa620e21ad30cf6d73344f7f2b
Jupixe <hell.ya.bigdeli.1375@gmail.com>
2023-10-20 16:37:28 +0330

    fix

--- src/utils/reducer/reducer.ts
+++ src/utils/reducer/reducer.ts
@@ -1,30 +1,31 @@
 import { toast } from "react-toastify";
 
-export const detailsReducer = (state, action) => {
-    switch ( action.type) {
-        case "projectModal" :
-            return {...state, projectModal : !state.projectModal};
-        case "nameEdit" :
-            return {...state, nameEdit : !state.nameEdit};
-        case "colorEdit" :
-            return {...state, colorEdit : !state.colorEdit};
-        case "alert" :
-            return {...state, alert : !state.alert};
-        case "copyLink" :
-            {
-                navigator.clipboard.writeText(window.location.href);
-                toast.success("لینک با موفقیت در کلیپ بورد کپی شد.");
-                return ""
-            }
-        case "proAlert" : 
-            return {...state, proAlert : !state.proAlert};
-        case "share" :
-            return {...state, share : !state.share};
-        case "newTask" :
-            return {...state, newTask : !state.newTask};
-        case "proNameEdit" :
-            return {...state, proNameEdit : !state.proNameEdit};
-        default:
-            return state;
+const detailsReducer = (state, action) => {
+  switch (action.type) {
+    case "projectModal":
+      return { ...state, projectModal: !state.projectModal };
+    case "workspaceNameEdit":
+      return { ...state, workspaceNameEdit: !state.workspaceNameEdit };
+    case "projectNameEdit":
+      return { ...state, projectNameEdit: !state.projectNameEdit };
+    case "colorEdit":
+      return { ...state, colorEdit: !state.colorEdit };
+    case "projectAlert":
+      return { ...state, projectAlert: !state.projectAlert };
+    case "workspaceAlert":
+      return { ...state, workspaceAlert: !state.workspaceAlert };
+    case "share":
+      return { ...state, share: !state.share };
+    case "newTask":
+      return { ...state, newTask: !state.newTask };
+    case "copyLink": {
+      navigator.clipboard.writeText(window.location.href);
+      toast.success("لینک با موفقیت در کلیپ بورد کپی شد.");
+      return state;
     }
-}
+    default:
+      return state;
+  }
+};
+
+export { detailsReducer };

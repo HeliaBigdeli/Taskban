1c1733cdc95b84661d6b322d5ecfb7a7bdd543c9
unknown <f.e.h.farshad@gmail.com>
2023-10-26 00:43:32 +0330

    fix: remove localstorage checking for darkMode

--- src/components/Theme/Switcher/index.tsx
+++ src/components/Theme/Switcher/index.tsx
@@ -1,5 +1,5 @@
 import Icon from "../../Common/Icon";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { ThemeContext } from "../../../context/ThemeContext";
 
 const Switcher: React.FC = () => {
@@ -20,22 +20,14 @@ const Switcher: React.FC = () => {
       <span
         onClick={handleClick}
         className={`flex items-center ${
-          isDarkTheme === false && localStorage.getItem("theme") === "light"
-            ? "justify-end"
-            : "justify-start"
+          isDarkTheme === false ? "justify-end" : "justify-start"
         } w-[65px] bg-lightgray_200 rounded-md cursor-pointer`}
       >
         <Icon
-          icon={
-            isDarkTheme === false && localStorage.getItem("theme") === "light"
-              ? "sun"
-              : "moon"
-          }
+          icon={isDarkTheme === false ? "sun" : "moon"}
           color="#818181"
           className={`${
-            isDarkTheme === false && localStorage.getItem("theme") === "light"
-              ? "bg-white"
-              : "bg-[#323232]"
+            isDarkTheme === false ? "bg-white" : "bg-[#323232]"
           } m-1 rounded-md p-[3px]`}
         />
       </span>
